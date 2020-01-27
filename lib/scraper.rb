0f659ea5f40551aebafb0ceb3c547072d590c678
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-27 12:05:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,5 +2,5 @@ require 'nokogiri'
 require 'open-uri'
  
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".headline-26OIBN")
-puts doc.text
+doc.css(".headline-26OIBN").text
+puts doc
